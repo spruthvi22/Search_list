bcf8574ca4ad395bb0bd0c14dca3f52195cb5b01
pruthvi <s.pruthvi22@gmail.com>
2019-11-06 20:10:30 +0000

    Using in operator membership expression

--- power.py
+++ power.py
@@ -7,9 +7,7 @@ Simple code to search list for 2**5
 L=[1,2,4,8,16,32,64]
 X=5
 
-for i in range(len(L)):
-    if 2**X==L[i]:
-        print('at index',i)
-        break
+if 2**X in L:
+    print('at index',L.index(2**X))
 else:
-    print(X,'not found')        
+    print(X,"not found")
